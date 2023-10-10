5ae6b234d2f5a4e6695152efd3a0cec35855f0f5
omiras <oscar.miras@gmail.com>
2023-10-10 08:37:42 +0200

    ex3 screenshot

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Algunos ejercicios para practicar con bucles.
 
-Propuesta de solución de [Samaneh](https://github.com/samane6866/loop-playground)
+Propuesta de solución de [Eduard](https://github.com/GoatBass/loop-playground)
 
 Nota: Para el ejercicio 0 tenéis que aprender a usar un condicional que no hemos dado. [switch](https://www.w3schools.com/js/js_switch.asp)
 
--- ex3.js
+++ ex3.js
@@ -1,22 +1,26 @@
 /**
  * Ejercicio 3: ¿En que iteración me encuentro?.
- * 
+ *
  * Escribe un mensaje por el terminal. Un mensaje para cada iteración, indicando en que iteración se encuentra el bucle en este momento.
- * 
+ *
  * Ejemplo:
- * 
+ *
  * "Estoy en la iteación 0"
  * "Estoy en la iteración 1"
  * .
  * .
  * .
- * "Estoy en la iteración 10"
- * 
- * Piensa qué variable guarda la iteración en la que nos encotramos
- * 
+ * "Estoy en la iteración 9"
+ *
+ * Piensa qué variable guarda la iteración en la que nos encontramos
+ *
  */
 
-for (var i=0;i<10;i++) {
-    // aquí tu código
-    console.log("Estoy en la iteración i")
+for (var i = 0; i < 10; i++) {
+  // aquí tu código
+  console.log("Estoy en la iteración i");
 }
+
+/**
+ * Resultado esperado: https://oscarm.tinytake.com/msc/ODc4MzU3OV8yMjIwMzgxMg
+ */
